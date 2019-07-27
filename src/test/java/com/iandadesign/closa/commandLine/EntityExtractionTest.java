20915e1e3ec23d8fa18f3d01edf847eda781245e
fabianmarquart <Donaldo1993>
2019-07-27 09:50:33 +0200

    Maven additional test directory

--- src/test/java/com/iandadesign/closa/commandLine/EntityExtractionTest.java
+++ src/test/java/com/iandadesign/closa/commandLine/EntityExtractionTest.java
@@ -1,6 +1,9 @@
 package com.iandadesign.closa.commandLine;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.concurrent.*;
 
 public class EntityExtractionTest {
 
@@ -8,18 +11,26 @@ public class EntityExtractionTest {
     public void mainTest() {
         String inputPath = "src/test/resources/com/iandadesign/closa/test.txt";
         String outputPath = "src/test/resources/com/iandadesign/closa/test-output.txt";
-        WikidataEntityExtraction.main(new String[]{"-i", inputPath, "-o", outputPath});
+
+        this.executeWithTimeOut(() -> {
+            WikidataEntityExtraction.main(new String[]{"-i", inputPath, "-o", outputPath});
+            return new Object();
+        });
     }
 
     @Test
     public void mainParametersTest() {
         String inputPath = "src/test/resources/com/iandadesign/closa/test.txt";
         String outputPath = "src/test/resources/com/iandadesign/closa/test-output.txt";
-        WikidataEntityExtraction.main(new String[]{
-                "-i", inputPath,
-                "-o", outputPath,
-                "-t", "neutral", "biology", "fiction",
-                "-l", "en", "es", "fr"});
+
+        this.executeWithTimeOut(() -> {
+            WikidataEntityExtraction.main(new String[]{
+                    "-i", inputPath,
+                    "-o", outputPath,
+                    "-t", "neutral", "biology", "fiction",
+                    "-l", "en", "es", "fr"});
+            return new Object();
+        });
     }
 
 
@@ -27,11 +38,29 @@ public class EntityExtractionTest {
     public void mainAnnotationTest() {
         String inputPath = "src/test/resources/com/iandadesign/closa/test.txt";
         String outputPath = "src/test/resources/com/iandadesign/closa/test-output.txt";
-        WikidataEntityExtraction.main(new String[]{
-                "-i", inputPath,
-                "-o", outputPath,
-                "-t", "neutral", "biology", "fiction",
-                "-l", "en", "es", "fr",
-                "-a"});
+
+        this.executeWithTimeOut(() -> {
+            WikidataEntityExtraction.main(new String[]{
+                    "-i", inputPath,
+                    "-o", outputPath,
+                    "-t", "neutral", "biology", "fiction",
+                    "-l", "en", "es", "fr",
+                    "-a"});
+            return new Object();
+        });
+    }
+
+
+    private void executeWithTimeOut(Callable<Object> task) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Future<Object> future = executor.submit(task);
+        try {
+            future.get(5, TimeUnit.MINUTES);
+        } catch (TimeoutException | InterruptedException | ExecutionException e) {
+            Assert.fail();
+            e.printStackTrace();
+        } finally {
+            future.cancel(true); // may or may not desire this
+        }
     }
 }
