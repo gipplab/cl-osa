35a04575961cd0b215ef68e31244f85b05c34945
fabianmarquart <Donaldo1993>
2019-08-08 17:57:07 +0200

    Article count by language

--- src/main/java/com/iandadesign/closa/evaluation/impl/CLESAEvaluationSet.java
+++ src/main/java/com/iandadesign/closa/evaluation/impl/CLESAEvaluationSet.java
@@ -114,7 +114,7 @@ public class CLESAEvaluationSet extends EvaluationSet<Double> {
 
                 System.out.println("Current language = " + language);
 
-                if (articleCollection.find(new Document("text." + language, "$exists")).iterator().hasNext()) {
+                if (articleCollection.find(new Document("text." + language, new Document("$exists", true))).first() != null) {
                     System.out.println("Language present.");
                     continue;
                 }
