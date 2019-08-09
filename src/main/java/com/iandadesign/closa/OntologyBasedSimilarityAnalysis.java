f8db9efb000b57afff407467f50eb2f7ffa1fdab
fabianmarquart <Donaldo1993>
2019-08-09 09:42:09 +0200

    Whitespace regex text

--- src/main/java/com/iandadesign/closa/OntologyBasedSimilarityAnalysis.java
+++ src/main/java/com/iandadesign/closa/OntologyBasedSimilarityAnalysis.java
@@ -20,6 +20,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 
