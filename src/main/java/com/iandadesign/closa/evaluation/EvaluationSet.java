aa7de7fba568cafaa6bcc3755c08ff7784e2173c
fabianmarquart <Donaldo1993>
2019-08-16 17:07:45 +0200

    CL-ESA parallel

--- src/main/java/com/iandadesign/closa/evaluation/EvaluationSet.java
+++ src/main/java/com/iandadesign/closa/evaluation/EvaluationSet.java
@@ -205,7 +205,7 @@ public abstract class EvaluationSet<T> {
 
         List<File> keys = new ArrayList<>(files.keySet());
 
-        ForkJoinPool customThreadPool = new ForkJoinPool(8);
+        ForkJoinPool customThreadPool = new ForkJoinPool(4);
 
         List<Integer> integers = IntStream.range(0, keys.size())
                 .sorted()
