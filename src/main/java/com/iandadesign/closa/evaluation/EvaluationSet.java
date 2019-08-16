51232df8aadf7d50b7a9d65391044a7bef233194
fabianmarquart <Donaldo1993>
2019-08-16 11:36:36 +0200

    CL-ESA 8 parallelism

--- src/main/java/com/iandadesign/closa/evaluation/EvaluationSet.java
+++ src/main/java/com/iandadesign/closa/evaluation/EvaluationSet.java
@@ -205,7 +205,7 @@ public abstract class EvaluationSet<T> {
 
         List<File> keys = new ArrayList<>(files.keySet());
 
-        ForkJoinPool customThreadPool = new ForkJoinPool(16);
+        ForkJoinPool customThreadPool = new ForkJoinPool(8);
 
         List<Integer> integers = IntStream.range(0, keys.size())
                 .sorted()
