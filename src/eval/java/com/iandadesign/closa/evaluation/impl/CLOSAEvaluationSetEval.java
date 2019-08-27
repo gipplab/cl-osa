69a533e1cbfd04fd88b8d0099fbf9ca34a92ea90
fabianmarquart <Donaldo1993>
2019-08-27 16:55:34 +0200

    Output

--- src/eval/java/com/iandadesign/closa/evaluation/impl/CLOSAEvaluationSetEval.java
+++ src/eval/java/com/iandadesign/closa/evaluation/impl/CLOSAEvaluationSetEval.java
@@ -135,7 +135,7 @@ class CLOSAEvaluationSetEval {
             CLOSAEvaluationSet englishJapaneseASPECEvaluationSetCLOSA = new CLOSAEvaluationSet(
                     new File(System.getProperty("user.home") + "/ASPECx/ja"), "ja",
                     new File(System.getProperty("user.home") + "/ASPECx/en"), "en",
-                    5
+                    10
             );
 
         englishJapaneseASPECEvaluationSetCLOSA.setGraphBasedAnalysis(true);
