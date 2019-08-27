c55af9984795ac4b4687d9eac8f8db697eabdc9f
fabianmarquart <Donaldo1993>
2019-08-28 00:07:15 +0200

    Output

--- src/main/java/com/iandadesign/closa/OntologyBasedSimilarityAnalysis.java
+++ src/main/java/com/iandadesign/closa/OntologyBasedSimilarityAnalysis.java
@@ -269,7 +269,7 @@ public class OntologyBasedSimilarityAnalysis {
 
                 double similarity = WikidataSimilarityUtil.cosineSimilarity(suspiciousEntry.getValue(), candidateEntry.getValue());
 
-                if (similarity > 0.35) {
+                if (similarity > 0.29) {
                     System.out.println();
                     System.out.println(suspiciousEntry.getKey());
                     System.out.println(candidateEntry.getKey());
