d6105ae657d6c3b233a39840f4992374d5460ce2
Fabian Marquart <fabianmarquart93@gmail.com>
2019-12-09 15:16:17 +0000

    config

--- wikipedia_download.sh
+++ wikipedia_download.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+mkdir /data/wikipedia
+
 for LANG in es en de zh ja fr
 do
-	wget https://dumps.wikimedia.org/${LANG}wiki/20190801/${LANG}wiki-20190801-pages-articles-multistream.xml.bz2 -O ~/wikipedia/output_${LANG}
+	wget https://dumps.wikimedia.org/${LANG}wiki/20191201/${LANG}wiki-20191201-pages-articles-multistream.xml.bz2 -O /data/wikipedia/output_${LANG}
 done
 
