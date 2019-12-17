07cf2a475bd920114618c0713b73a1bfce6d85bf
Fabian Marquart <fabianmarquart93@gmail.com>
2019-12-17 16:41:48 +0100

    CL-ESA

--- wikipedia_download.sh
+++ wikipedia_download.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 mkdir /data/wikipedia
 
-for LANG in es en de zh ja fr
+for LANG in es #en zh ja fr
 do
-	wget https://dumps.wikimedia.org/${LANG}wiki/20191201/${LANG}wiki-20191201-pages-articles-multistream.xml.bz2 -O /data/wikipedia/output_${LANG}
+	wget https://dumps.wikimedia.org/${LANG}wiki/20191201/${LANG}wiki-20191201-pages-articles-multistream.xml.bz2 -O /data/wikipedia/dump_${LANG}.xml.bz2
 done
 
+for LANG in es #en zh ja fr
+do
+  python ./WikiExtractor.py /data/wikipedia/dump_${LANG}.xml.bz2
+done
