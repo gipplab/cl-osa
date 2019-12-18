696c810e33157ab120495888f91edd9eb7515170
Fabian Marquart <fabianmarquart93@gmail.com>
2019-12-18 10:39:46 +0100

    CL-ESA

--- wikipedia_download.sh
+++ wikipedia_download.sh
@@ -8,5 +8,5 @@ done
 
 for LANG in es en zh ja fr
 do
-  python ./WikiExtractor.py -o /data/wikipedia/output_${LANG} /data/wikipedia/dump_${LANG}.xml.bz2
+  python3 ./WikiExtractor.py -o /data/wikipedia/output_${LANG} /data/wikipedia/dump_${LANG}.xml.bz2
 done
