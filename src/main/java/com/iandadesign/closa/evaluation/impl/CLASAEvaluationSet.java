82b7978698d55a9284a3e3b0197eec70f77c7df6
Fabian Marquart <fabianmarquart93@gmail.com>
2020-01-03 16:10:47 +0100

    CL-ASA reuse parts of files when document count is lower revert

--- src/main/java/com/iandadesign/closa/evaluation/impl/CLASAEvaluationSet.java
+++ src/main/java/com/iandadesign/closa/evaluation/impl/CLASAEvaluationSet.java
@@ -239,7 +239,7 @@ public class CLASAEvaluationSet extends EvaluationSet<String> {
 
                             try {
                                 if (Files.exists(probabilitiesFilePath) &&
-                                        FileUtils.readLines(probabilitiesFile, StandardCharsets.UTF_8).size() >= candidateIdTokensMap.size()) {
+                                        FileUtils.readLines(probabilitiesFile, StandardCharsets.UTF_8).size() == candidateIdTokensMap.size()) {
 
                                     List<String> lines = FileUtils.readLines(probabilitiesFile, StandardCharsets.UTF_8)
                                             .stream()
