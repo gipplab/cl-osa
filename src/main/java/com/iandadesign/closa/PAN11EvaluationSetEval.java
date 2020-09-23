3e1eac11eaa1821c4153e65a3f5d917c2635c47e
Johannes <johannesst@gmx.de>
2020-09-23 11:34:15 +0200

    Path set for evaluation.

--- src/main/java/com/iandadesign/closa/PAN11EvaluationSetEval.java
+++ src/main/java/com/iandadesign/closa/PAN11EvaluationSetEval.java
@@ -15,6 +15,7 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -294,8 +295,10 @@ public class PAN11EvaluationSetEval {
                 }
             }
 
-            // This works if the last iteration is ok
-            baseResultsPath = osa.getExtendedXmlResultsPath();
+            // Generating xmlResultsfolderPath as it is done in each thread atm.
+            String xmlResultsFolderPath = Paths.get(osa.getPreprocessedCachingDirectory(), "preprocessed_extended",
+                    "results_comparison", tag.concat("_").concat(logUtil.getDateString())).toAbsolutePath().toString();
+            baseResultsPath = xmlResultsFolderPath;
             parsedFiles = parsedFilesP.get();
             parsedErrors = parsedErrorsP.get();
         }
