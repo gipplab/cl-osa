c25f8e41985184f2ebdc82a298888b1bdaf32704
Johannes <johannesst@gmx.de>
2020-12-18 22:28:44 +0100

    doASalvadorRoll.sh upgraded

--- doASalvadorRoll.sh
+++ doASalvadorRoll.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "Please give Purpose as filename and retry..."
+    exit 1
+fi
+
+mkdir -p ./mylogs
 cd /cl-osa-tng && mvn package -DskipTests -Dmaven.javadoc.skip=true
-java -cp /cl-osa-tng/target/closa-1.4.jar com.iandadesign.closa.SalvadorFragmentLevelEval
+java -cp /cl-osa-tng/target/closa-1.4.jar com.iandadesign.closa.SalvadorFragmentLevelEval 2>&1 | tee ./mylogs/$1
