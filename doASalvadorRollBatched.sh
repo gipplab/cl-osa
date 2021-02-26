b08b6b2627abf3995a612bd76e400156a03f17ea
Johannes <johannesst@gmx.de>
2021-02-26 20:45:45 +0100

    New batch settings.

--- doASalvadorRollBatched.sh
+++ doASalvadorRollBatched.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-# find best THRESH1 with 6-3
-# find best mergemode + THRESH2 with 6-3
+# find best THRESH1 with 6-3: Seems to be roughly 1400, but not much difference
+# find best mergemode + THRESH2 with 6-3:
+# plausibility check small fragment small thresh, big big tresh etc once or twice
 # compare this with absolute scoring once
 # compare with other fragmentations 6-3 8-4? 10-5 14-7
-# maybe do de comparison check with best config
-# maybe do de
+# maybe do EN-DE comparison check with best config
+
 
 
 # batchBaseName / USE_ABSOLUTE_SCORES / THRESH1 / THRESH2 / FRAGMENT_MERGE_MODE
 BATCH_BASE_NAME=batchTest
-./doASalvadorRoll.sh $BATCH_BASE_NAME false 2400 1.15  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME false 2400 1.20  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME false 2400 1.25  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME false 2400 1.30  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME false 2400 1.35  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME false 2400 1.40  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME false 2400 1.50  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME false 2400 1.55  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME false 2400 1.60  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME false 1400 0.10  keepingMax
+./doASalvadorRoll.sh $BATCH_BASE_NAME false 1400 0.20  keepingMax
+./doASalvadorRoll.sh $BATCH_BASE_NAME false 1400 0.30  keepingMax
+./doASalvadorRoll.sh $BATCH_BASE_NAME false 1400 0.40  keepingMax
+./doASalvadorRoll.sh $BATCH_BASE_NAME false 1400 0.50  keepingMax
+./doASalvadorRoll.sh $BATCH_BASE_NAME false 1400 0.60  keepingMax
+./doASalvadorRoll.sh $BATCH_BASE_NAME false 1400 0.70  keepingMax
+./doASalvadorRoll.sh $BATCH_BASE_NAME false 1400 0.80  keepingMax
+
+
