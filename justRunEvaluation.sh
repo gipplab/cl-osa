9c4f8db83a748e7c2efe34745e52e433c25c9497
Johannes <johannesst@gmx.de>
2021-03-13 19:58:34 +0100

    just trigger evaluation script2

--- justRunEvaluation.sh
+++ justRunEvaluation.sh
@@ -5,8 +5,8 @@ PLAGPATH="/data/CLOSA_data/preprocessed/preprocessed_extended/results_comparison
 DETPATH="/data/CLOSA_data/preprocessed/preprocessed_extended/results_comparison_salvador/evalPAN2011En-es_2021_03_13_19_43_51"
 
 
-python  ./src/main/resources/pan-pc11/pan09-plagiarism-detection-perfomance-measures-modified.py $PLAGPATH det-path=$DETPATH
-python  ./src/main/resources/pan-pc11/pan09-plagiarism-detection-perfomance-measures-modified.py $PLAGPATH det-path=$DETPATH filter="onlyShortCases"
-python  ./src/main/resources/pan-pc11/pan09-plagiarism-detection-perfomance-measures-modified.py $PLAGPATH det-path=$DETPATH filter="onlyMediumCases"
-python  ./src/main/resources/pan-pc11/pan09-plagiarism-detection-perfomance-measures-modified.py $PLAGPATH det-path=$DETPATH filter="onlyLongCases"
+python  ./src/main/resources/pan-pc11/pan09-plagiarism-detection-perfomance-measures-modified.py -p $PLAGPATH det-path=$DETPATH
+python  ./src/main/resources/pan-pc11/pan09-plagiarism-detection-perfomance-measures-modified.py -p $PLAGPATH det-path=$DETPATH filter="onlyShortCases"
+python  ./src/main/resources/pan-pc11/pan09-plagiarism-detection-perfomance-measures-modified.py -p $PLAGPATH det-path=$DETPATH filter="onlyMediumCases"
+python  ./src/main/resources/pan-pc11/pan09-plagiarism-detection-perfomance-measures-modified.py -p $PLAGPATH det-path=$DETPATH filter="onlyLongCases"
 
