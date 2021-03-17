7dc601a6bab79644d805eee68af80143b39ec5c2
Johannes <johannesst@gmx.de>
2021-03-17 12:02:06 +0100

    fix skewed representative dataset

--- src/main/java/com/iandadesign/closa/util/PAN11FileUtil.java
+++ src/main/java/com/iandadesign/closa/util/PAN11FileUtil.java
@@ -173,7 +173,7 @@ public class PAN11FileUtil {
             }
 
             List<File> newSuspiciousFiles = new ArrayList<>();
-            /*
+
             newSuspiciousFiles.add(suspiciousFiles.get(1));
             newSuspiciousFiles.add(suspiciousFiles.get(70));
             newSuspiciousFiles.add(suspiciousFiles.get(124));
@@ -181,7 +181,7 @@ public class PAN11FileUtil {
             newSuspiciousFiles.add(suspiciousFiles.get(197));
             newSuspiciousFiles.add(suspiciousFiles.get(198));
             newSuspiciousFiles.add(suspiciousFiles.get(290));
-            */
+
             newSuspiciousFiles.add(suspiciousFiles.get(291));
 
 
