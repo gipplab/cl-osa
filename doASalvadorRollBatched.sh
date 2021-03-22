eb0977b8fd003e92888c6fbbac48084d4929b997
Johannes <johannesst@gmx.de>
2021-03-22 15:00:01 +0100

    batchtestfix

--- doASalvadorRollBatched.sh
+++ doASalvadorRollBatched.sh
@@ -2,7 +2,7 @@
 # find best THRESH1 with 6-3: Seems to be roughly 1400, but not much difference
 # find best mergemode + THRESH2 with 6-3 (seems to be 1400, simpleAdd)
 # compare this with absolute scoring once = absolute scoring has much higher scoring
-# find best THRESH2 for absolute scoring
+# find best THRESH2 for absolute scoring - ok
 # plausibility check small fragment small thresh, big big thresh etc once or twice
 # compare with other fragmentations 6-3 8-4? 10-5 14-7
 # maybe do EN-DE comparison check with best config
@@ -10,14 +10,27 @@
 
 
 # batchBaseName / USE_ABSOLUTE_SCORES / THRESH1 / THRESH2 / FRAGMENT_MERGE_MODE
-BATCH_BASE_NAME=batchTestAbsolute
-./doASalvadorRoll.sh $BATCH_BASE_NAME true 1400 9  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME true 1400 8  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME true 1400 7  simpleAdd
+BATCH_BASE_NAME=batch6-3-SP-Rel-
 
-./doASalvadorRoll.sh $BATCH_BASE_NAME true 2400 13  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME true 2400 12  simpleAdd
-./doASalvadorRoll.sh $BATCH_BASE_NAME true 2400 10  simpleAdd
+
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 1400 0.522  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 1400 0.586  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 1400 0.622  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 1400 0.686  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 1400 0.722  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 1400 0.786  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 1400 0.822  simpleAdd
+
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 2400 0.522  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 2400 0.586  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 2400 0.622  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 2400 0.686  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 2400 0.722  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 2400 0.786  simpleAdd
+./doASalvadorRoll.sh $BATCH_BASE_NAME true 2400 0.822  simpleAdd
+
+
+
 
 
 # Best Thresh2
